7241d3961edfb36943d6150b3dc17a700bf6d6b2
MathieuGermain <gtavoverride@gmail.com>
2022-09-13 12:15:58 -0400

    Program entry file now use environment variables

--- backend-app/src/index.ts
+++ backend-app/src/index.ts
@@ -1,5 +1,13 @@
+import { config } from 'dotenv';
 import { EventReminderApplication } from './app/application';
-const app = new EventReminderApplication('main-service');
+
+// Initialize Env Variables
+config();
+
+// Initialize Event Reminder App
+const app = new EventReminderApplication(process.env.SERVICE_ID ? process.env.SERVICE_ID : 'default-service');
 
 // Start application then start the service
-app.start(3333, 'localhost').then(() => app.EventReminderService.start());
+app.start(process.env.PORT ? Number(process.env.PORT) : 3333, process.env.HOST ? process.env.HOST : 'localhost').then(
+    () => app.EventReminderService.start(),
+);
