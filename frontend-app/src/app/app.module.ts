47e9bb8761cac5ab37dd6a77cef135306fa152fe
MathieuGermain <gtavoverride@gmail.com>
2022-09-14 14:16:19 -0400

    Revert "Update app.module.ts"
    This reverts commit 54c520e24ee1cba262d664f524f76a4412febd79.

--- frontend-app/src/app/app.module.ts
+++ frontend-app/src/app/app.module.ts
@@ -1,42 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { EventListComponent } from './components/event-list/event-list.component';
-import { EventItemComponent } from './components/event-item/event-item.component';
-import { CustomButtonComponent } from './components/custom-button/custom-button.component';
-import { InViewDirective } from './directives/in-view.directive';
-import { OrderByPipe } from './pipes/order-by.pipe';
-
-const config: SocketIoConfig = {
-  url: 'http://localhost:3344',
-  options: {
-    autoConnect: false,
-    reconnection: true
-  }
-};
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    NavbarComponent,
-    EventListComponent,
-    EventItemComponent,
-    CustomButtonComponent,
-    InViewDirective,
-    OrderByPipe
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    BrowserModule, SocketIoModule.forRoot(config)
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
