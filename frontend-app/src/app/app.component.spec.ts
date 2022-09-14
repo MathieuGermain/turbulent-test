05e2e67ab1c8aa3e33f1d0fb38e9c8647bad785a
MathieuGermain <gtavoverride@gmail.com>
2022-09-13 21:49:49 -0400

    Update AppComponent

--- frontend-app/src/app/app.component.spec.ts
+++ frontend-app/src/app/app.component.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { EventReminderService, EventReminderServiceMock } from './services/event-reminder-service.service';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
+      ],
+      providers: [
+        { provide: EventReminderService, useValue: new EventReminderServiceMock() }
       ],
       declarations: [
         AppComponent
@@ -18,11 +22,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'Event Reminder'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('Event Reminder');
   });
 });
