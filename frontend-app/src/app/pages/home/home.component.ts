f9342bcbf32bdfe7c62b612dbffa2bd72b4e3649
MathieuGermain <gtavoverride@gmail.com>
2022-09-13 21:48:50 -0400

    Add home page

---
+++ frontend-app/src/app/pages/home/home.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { EventReminderService } from 'src/app/services/event-reminder-service.service';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(private eventReminder: EventReminderService) { }
+
+  public get events() {
+    return this.eventReminder.Events;
+  }
+
+  ngOnInit(): void {
+
+  }
+
+}
