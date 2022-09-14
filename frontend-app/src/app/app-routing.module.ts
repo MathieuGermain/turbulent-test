2b249e2cafe7fb3156a96241c43796239dc3fcef
MathieuGermain <gtavoverride@gmail.com>
2022-09-14 14:16:32 -0400

    Revert "Add app routing"
    This reverts commit f9be13551e2b60e3804a31f3ea9e82995d6ad767.

--- frontend-app/src/app/app-routing.module.ts
+++ frontend-app/src/app/app-routing.module.ts
@@ -1,17 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [
-  {
-    path: 'home',
-    component: HomeComponent
-  },
-  {
-    path: '**',
-    redirectTo: '/home'
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
