f8946b4459046fb26ab0ddb60bf73c0bd8c7fa4c
MathieuGermain <gtavoverride@gmail.com>
2022-09-15 00:34:24 -0400

    Changed main route to /

--- frontend-app/src/app/app-routing.module.ts
+++ frontend-app/src/app/app-routing.module.ts
@@ -4,12 +4,12 @@ import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   {
-    path: 'home',
+    path: '',
     component: HomeComponent
   },
   {
     path: '**',
-    redirectTo: '/home'
+    redirectTo: ''
   }
 ];
 
