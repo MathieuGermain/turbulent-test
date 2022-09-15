e636b6dd9697697985fe29705c531f5b67bef265
MathieuGermain <gtavoverride@gmail.com>
2022-09-15 15:08:42 -0400

    Add index comments

--- backend-app/src/index.ts
+++ backend-app/src/index.ts
@@ -1,15 +1,20 @@
 import { config } from 'dotenv';
 import { EventReminderApplication } from './app/application';
 
-// Initialize Env Variables
+// Config .env variables
 config();
 
+// Parse .env variables or use default values
 const SERVICE_ID = process.env.SERVICE_ID ? process.env.SERVICE_ID : 'default-service';
 const PORT = process.env.PORT ? Number(process.env.PORT) : 3333;
 const HOST = process.env.HOST ? process.env.HOST : 'localhost';
 
-// Initialize Event Reminder App
+// Instanciate EventReminderApplication
 const app = new EventReminderApplication(SERVICE_ID);
 
-// Start application then start the service
+/**
+ * Start application then start the service
+ * - We start listening on the http and socket server
+ * - We then start our service process
+ */
 app.start(PORT, HOST).then(() => app.EventReminderService.start());
